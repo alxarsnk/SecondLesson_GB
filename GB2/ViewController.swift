8c362d0d887d1a1a32570c8e1496fe81006cef40
Alexandr Arsenuk <alexandr.arsenuk@devexperts.com>
2021-11-19 20:30:45 +0300

    Initial Commit

---
+++ GB2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GB2
+//
+//  Created by Александр Арсенюк on 19.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
